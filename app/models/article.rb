038f63955cdd19098fd2d17f0be246a48f336a1b
Kurnia Muhamad <omenkzz@gmail.com>
2024-06-19 19:13:27 +0700

    feat: delete comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  
   validates :title, presence: true, length:  { minimum: 10,   maximum: 40}
   validates :body, presence: true, length: { minimum: 10 }
 end
